2644a0e0e8d35a1b0931d5baf1b5d3a87b85ef80
Vincent Chenzhun Huang <39947361+vincent-chenzhun-huang@users.noreply.github.com>
2023-03-27 19:50:46 -0400

    Remove assertion only

--- generate.py
+++ generate.py
@@ -9,7 +9,8 @@ from peft import PeftModel
 # assert (
 #     "LlamaTokenizer" in transformers._import_structure["models.llama"]
 # ), "LLaMA is now in HuggingFace's main branch.\nPlease reinstall it: pip uninstall transformers && pip install git+https://github.com/huggingface/transformers.git"  # noqa: E501
-# from transformers import GenerationConfig, LlamaForCausalLM, LlamaTokenizer
+
+from transformers import GenerationConfig, LlamaForCausalLM, LlamaTokenizer
 
 if torch.cuda.is_available():
     device = "cuda"
